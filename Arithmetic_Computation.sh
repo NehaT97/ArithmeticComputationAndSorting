02bdd7f9729f1c0a32f36024d85e208a44fa1089
unknown <nehathakur764@gmail.com>
2020-10-12 14:07:37 +0530

    Second Operation

--- Arithmetic_Computation.sh
+++ Arithmetic_Computation.sh
@@ -7,11 +7,19 @@ read -p "B:" b
 read -p "C:" c
 
 #Display Value 
+echo ""
 echo "A:" $a
 echo "B:" $b
 echo "C:" $c
+echo ""
 
 #FirstOperation
-
 let firstresult=($a+$b)*$c
 echo "FirstResult:" $firstresult
+echo ""
+
+#SecondOperation
+let secondresult=($a*$b)+$c
+echo "secondResult:" $secondresult
+echo ""
+
